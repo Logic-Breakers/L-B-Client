7849800f75e333c987a3ed944f6d08b00a2b9e9c
김우주 <https://github.com/ghvhdh333>
2023-12-14 17:07:08 +0900

    mkdir userInfoPage

---
+++ my-app/pages/users/show.js
@@ -0,0 +1,13 @@
+import Title from "@/components/Title"
+import Header_Account from "@/components/Header/Header_Account"
+
+// 나중에 파일명을 동적으로 변환해야할 듯 (유저 id값마다 보여줘야하기에...)
+export default function UserInfo(){
+    return (
+        <>
+            <Title text={'게스트 프로필 - 에어비앤비'}/>
+            <Header_Account />
+            유저 정보 페이지
+        </>
+    )
+}
