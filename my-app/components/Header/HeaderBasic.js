3a0a24b5c37ab76bf8b77fca8b9ae1783c19e5be
김우주 <https://github.com/ghvhdh333>
2023-12-19 17:39:06 +0900

    edit/파일명

--- my-app/components/Header/HeaderBasic.js
+++ my-app/components/Header/HeaderBasic.js
@@ -1,12 +1,9 @@
 import Link from "next/link";
 import Nav from "./Nav/NavWeb";
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
 
 // 계정관리, 위시리스트, 메시지, 여행 페이지 등에 사용되는 헤더
-// w >= 1128px : 아이콘 + airbnb (px-[80px])
-// w < 1128px : 아이콘 (px-[40px])
 export default function HeaderBasic() {
     return (
         <header className="bnb_sm:hidden bg-[#fff] border-b-[1px] border-solid border-[#eeeeee] h-[80px] bnb_xl:px-[80px] px-[40px] flex flex-row justify-between items-center fixed top-0 left-0 w-[100%] z-[100]">
