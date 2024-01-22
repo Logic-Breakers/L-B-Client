3e94fb75f3e3dca50d1b632ed45eb33e37cba629
toystory23 <ghlduq@naver.com>
2024-01-22 20:53:08 +0900

    메인페이지 필터 및 화살표 이미지 구현

--- my-app/components/Header/Nav/MainMenu/MainNav.js
+++ my-app/components/Header/Nav/MainMenu/MainNav.js
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
+import { faCircleRight } from '@fortawesome/free-regular-svg-icons';
+import {faSliders} from '@fortawesome/free-solid-svg-icons'
 
 export default function MainNav() {
   const userFilter = [
@@ -23,13 +25,24 @@ export default function MainNav() {
     '사막',
   ];
   return (
-    <div className="flex justify-around">
-      {userFilter.map((item) => (
-        <div key={1} className="flex flex-col mt-4 cursor-pointer mb-5">
-          <FontAwesomeIcon icon={faImage} size='2x'/>
-          {item}
+    <div className="flex">
+      <div className="flex justify-around w-full">
+        {userFilter.map((item) => (
+          <div key={1} className="flex flex-col mt-4 cursor-pointer mb-5">
+            <FontAwesomeIcon icon={faImage} size="2x" />
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="flex">
+        <div className="flex items-center mx-2 cursor-pointer">
+          <FontAwesomeIcon icon={faCircleRight} size="2x" />
         </div>
-      ))}
+        <div className='flex items-center ml-4'>
+          <div className="flex justify-between border-2 items-center border-gray-400 rounded-md w-[6.5rem] h-14 px-4">
+          <FontAwesomeIcon icon={faSliders} size='2x' />필터</div>
+        </div>
+      </div>
     </div>
   );
 }
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,16 +1,14 @@
-import HeaderBasic from '@/components/Header/HeaderBasic';
-import MainHeader from '@/components/Header/MainHeader';
 import MainItems from '@/components/items/MainItems';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
 import Nav from '@/components/Header/Nav/Nav';
 import MainNav from '@/components/Header/Nav/MainMenu/MainNav';
 
 export default function Home() {
   return (
     <div className=" mx-24">
-      {/* <MainHeader /> */}
-      {/* <HeaderBasic/> */}
       <div className="flex justify-center items-center font-bold h-[5rem] text-xl border-[1px] border-b-gray-200 underline">
         에어비앤비에서 사랑받고 있는 &rsquo;게스트 선호숙소&lsquo;에 대해 알아보기
       </div>
@@ -22,16 +20,42 @@ export default function Home() {
           <span className="text-[28px] font-[600]"> airbnb</span>
         </span>
         <div className="flex justify-between w-[16rem] text-black text-xl">
-          <span>숙소</span>
-          <span>체험</span>
-          <span>온라인 체험</span>
+          <button>숙소</button>
+          <button>체험</button>
+          <button>온라인 체험</button>
         </div>
         <span className=" text-black">
           <Nav />
         </span>
       </div>
       <div className="flex justify-center items-center h-40">
-        <div className='border-2 border-gray-400 w-[60rem] rounded-full h-20'></div>
+        <div className="flex border-2 border-gray-400 w-[60rem] rounded-full h-20">
+          <div className="flex w-[22rem] items-center">
+            <button className="flex flex-col border-r-2 border-gray-400 px-12 w-full">
+              <div>여행지</div>
+              <div className="text-gray-400">여행지 검색</div>
+            </button>
+          </div>
+          <div className="flex justify-between items-center w-[20rem]">
+            <div className='border-r-2 border-gray-400 w-[10rem] px-6'>
+              <div>체크인</div>
+              <div className="text-gray-400">날짜 추가</div>
+            </div>
+            <div className='border-r-2 border-gray-400 w-[10rem] px-6'>
+              <div>체크아웃</div>
+              <div className="text-gray-400">날짜 추가</div>
+            </div>
+          </div>
+          <div className="flex items-center w-[13rem]">
+            <div className='w-[12rem] px-[1rem]'>
+              <div>여행자</div>
+              <div className="text-gray-400">게스트 추가</div>
+            </div>
+          </div>
+          <div className='flex items-center w-14 px-4'>
+          <FontAwesomeIcon icon={faMagnifyingGlass} size='3x' />
+          </div>
+        </div>
       </div>
       <MainNav />
       <MainItems />
