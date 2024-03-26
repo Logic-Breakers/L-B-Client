5b90ec10e41dc9fd7c3db2eb4faa432422ca8fed
김우주 <https://github.com/ghvhdh333>
2024-03-26 17:38:56 +0900

    fix/category-scroll-btn

--- my-app/components/Header/CategoryItem/HeaderCategory.jsx
+++ my-app/components/Header/CategoryItem/HeaderCategory.jsx
@@ -1,32 +1,38 @@
 import HeaderCategoryItem from "./HeaderCategoryItem";
-import ScrollLeftArrowBtn from "@/components/Buttons/ScrollLeftArrowBtn";
-import ScrollRightArrowBtn from "@/components/Buttons/ScrollRightArrowBtn";
+import ScrollArrowBtn from "@/components/Buttons/ScrollArrowBtn";
 import FilterBtn from "@/components/Header/Nav/FilterBtn";
 
 import CategoryItemData from "@/components/Datas/CategoryItemData";
 
 // 메인페이지 헤더 하단에 있는 여러 카테고리들과 필터버튼
 export default function HeaderCategory({ getCategoryData, category }) {
-  const onClickScrollLeft = () => {
+  const onClickScrollBtn = (direction) => {
+    // js 사용 말고 useRef를 사용해서도 구현가능하다.
+    console.log(direction);
     const scrollTarget = document.querySelector("#scrollTarget");
-    scrollTarget.scrollTo({
+    // console.dir(scrollTarget.scrollLeft);
+    const scrollAmount =
+      direction === "Left"
+        ? -scrollTarget.offsetWidth
+        : +scrollTarget.offsetWidth;
+    scrollTarget.scrollBy({
       behavior: "smooth",
-      left: scrollTarget.scrollLeft - scrollTarget.offsetWidth,
+      left: scrollAmount,
     });
   };
 
-  const onClickScrollRight = () => {
-    const scrollTarget = document.querySelector("#scrollTarget");
-    scrollTarget.scrollTo({
-      behavior: "smooth",
-      left: scrollTarget.scrollLeft + scrollTarget.offsetWidth,
-    });
-  };
+  // const onClickScrollRight = () => {
+  //   const scrollTarget = document.querySelector("#scrollTarget");
+  //   scrollTarget.scrollTo({
+  //     behavior: "smooth",
+  //     left: scrollTarget.scrollLeft + scrollTarget.offsetWidth,
+  //   });
+  // };
 
   return (
     <section className="bnb_xl:px-[80px] px-[40px] border-t-[1px] border-solid border-[#eeeeee] pt-3 flex flex-row justify-between items-center space-x-4">
       <div className="mr-2">
-        <ScrollLeftArrowBtn onClickScrollLeft={onClickScrollLeft} />
+        <ScrollArrowBtn onClickScrollBtn={onClickScrollBtn} dir={"Left"} />
       </div>
       {/* 카테고리 아이템들 */}
       <section
@@ -48,7 +54,7 @@ export default function HeaderCategory({ getCategoryData, category }) {
       <div className="flex flex-row justify-between items-center gap-x-8">
         {/* 오른쪽 화살표 버튼 */}
         <div className="ml-2">
-          <ScrollRightArrowBtn onClickScrollRight={onClickScrollRight} />
+          <ScrollArrowBtn onClickScrollBtn={onClickScrollBtn} dir={"Right"} />
         </div>
 
         {/* 필터 모달 버튼 */}
